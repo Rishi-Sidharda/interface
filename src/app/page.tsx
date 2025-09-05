cdd808538dd0f75a47b1d60ab7be4059966cc2f8
Rishi-Sidharda <rishisidharda@gmail.com>
2025-09-06 00:51:36 +0530

    added space for iframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,33 +26,38 @@ export default function ResizableColumns() {
   };
 
   return (
-    <div
-      className="flex w-screen h-screen select-none"
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
-    >
-      {/* Left panel */}
-      <div className={`bg-amber-100`} style={{ width: `${leftWidth}%` }}>
-        <div className="h-full flex flex-col">
-          <div className={`bg-fuchsia-200 flex-1`}></div>
-          <div className={`bg-red-200 flex-6`}></div>
-        </div>
+    <div>
+      <div className="absolute top-0 left-0 bg-gray-950 size-[100%] z-0 w-screen h-screen">
+        {/** KEEP IFRAME HERE */}
       </div>
-
-      {/* Divider */}
       <div
-        className="w-1 bg-gray-400 cursor-col-resize"
-        onMouseDown={() => setIsDragging(true)}
-      ></div>
+        className="flex w-screen h-screen select-none z-10 relative"
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      >
+        {/* Left panel */}
+        <div className={`bg-amber-100`} style={{ width: `${leftWidth}%` }}>
+          <div className="h-full flex flex-col">
+            <div className={`bg-fuchsia-200 flex-1`}></div>
+            <div className={`bg-red-200 flex-6`}></div>
+          </div>
+        </div>
 
-      {/* Middle panel */}
-      <div
-        className="bg-blue-300 flex-1"
-        style={{ width: `${100 - leftWidth - 30}%` }}
-      ></div>
+        {/* Divider */}
+        <div
+          className="w-1 bg-gray-400 cursor-col-resize"
+          onMouseDown={() => setIsDragging(true)}
+        ></div>
 
-      {/* Right panel */}
-      <div className="w-[22%] bg-green-200"></div>
+        {/* Middle panel */}
+        <div
+          className="bg-blue-300 flex-1 "
+          style={{ width: `${100 - leftWidth - 30}%` }}
+        ></div>
+
+        {/* Right panel */}
+        <div className="w-[22%] bg-green-200"></div>
+      </div>
     </div>
   );
 }
