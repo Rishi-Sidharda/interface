3272042823245e6cff3cb1720baa1bf8ae820af8
Rishi-Sidharda <rishisidharda@gmail.com>
2025-09-13 13:54:11 +0530

    no message

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,31 +6,31 @@ import HtmlCanvasRenderer from "@/components/HtmlCanvasRender";
 export default function HomePage() {
   const [html, setHtml] = useState(`
     <div style="display:flex; gap:8px; max-width:400px; margin:20px auto;">
-  <input 
-    type="search" 
-    placeholder="Search..." 
-    style="
-      flex:1; 
-      padding:8px 12px; 
-      border:1px solid #ccc; 
-      border-radius:6px; 
-      font-size:14px;
-    "
-  />
-  <button 
-    style="
-      padding:8px 14px; 
-      background:#6366f1; 
-      color:white; 
-      border:none; 
-      border-radius:6px; 
-      font-weight:600; 
-      cursor:pointer;
-    "
-  >
-    Search
-  </button>
-</div>
+      <input 
+        type="search" 
+        placeholder="Search..." 
+        style="
+          flex:1; 
+          padding:8px 12px; 
+          border:1px solid #ccc; 
+          border-radius:6px; 
+          font-size:14px;
+        "
+      />
+      <button 
+        style="
+          padding:8px 14px; 
+          background:#6366f1; 
+          color:white; 
+          border:none; 
+          border-radius:6px; 
+          font-weight:600; 
+          cursor:pointer;
+        "
+      >
+        Search
+      </button>
+  </div>
 
   `);
   return (
